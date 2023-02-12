5017630d432645cbf5cf7eb8d86c98384baf0c61
Lê Tất Chính <letatchinh123@gmail.com>
2023-02-12 23:40:45 +0700

    testtt

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import '../../firebase/firebase.js'
+import Header from 'components/home/Header.js'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  console.log(process.env.NEXT_PUBLIC_ANALYTICS_ID);
   return (
       <div className={styles.container}>
         <main className={styles.main}>
+        <Header />
           <h2>Home Page</h2>
         </main>
       </div>
