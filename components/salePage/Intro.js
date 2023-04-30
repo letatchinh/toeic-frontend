15b59f57006da17ff46a4ed1588d8035b8e4bfc8
Lê Tất Chính <letatchinh123@gmail.com>
2023-04-30 22:26:55 +0700

    first page

--- components/salePage/Intro.js
+++ components/salePage/Intro.js
@@ -7,7 +7,10 @@ export default function Intro() {
             <Image preview={false} src='/assets/images/salePage/teacher.png'/>
         </div>
         <div className='salePage--intro__text'>
-        <p>Tôi không muốn giỏi tiếnh anh</p>
+        <p className='classic'>"Tôi không muốn giỏi tiếnh anh"không ai nói như vậy cả</p>
+        <p className='bold'>Cách để có chứng chỉ toeic, cải thiện khả năng tiếng anh chỉ trong <span className='bgBlack'>5 tháng</span></p>
+        <p className='bold'> Dễ Dàng Nhất, Tiết Kiệm Nhất Và Hiệu Quả Nhất…</p>
+        <p className='classic'>Đây chính là thứ bạn tìm kiếm, tôi hứa, khám phá ở bài viết phía dưới.</p>
         </div>
     </div>
   )
